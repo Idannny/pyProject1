60c1e1e698ccb616896e305b05f18d8db1b6362d
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-09-11 19:17:54 -0700

    Conditional Statements - LaFrance
    I worked on if and else statements as well as some for and while loops. - LaFrance

---
+++ For_while_code.py
@@ -0,0 +1,22 @@
+# work on for and while statements:
+
+x = 100
+y = 200
+
+
+i = 0
+
+while i < x:
+    print(i)
+    i += 1  # i is incrementing by 1
+
+for j in range(y):
+
+    if j == 175: break
+    print(j)
+
+
+
+
+
+
---
+++ if_else_code.py
@@ -0,0 +1,20 @@
+# Working on creating an If and If else statement:
+
+
+x = 20 # some int x
+
+if x > 10:
+
+    print("x is greater than 10")
+
+# two more random ints to use in the conditional statements
+y = 100
+z = 200
+
+if y and z == 100:  # and, or, and not (!) are also used as comparisons in conditional statements
+
+    print("both y and z equal 100")
+
+else:
+    print("y and z equal different numbers")
+
